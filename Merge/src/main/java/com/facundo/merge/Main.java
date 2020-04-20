30076d5a73905d7b689f3a4620aaacb05004a8d1
candelaripoli <43279418+candelaripoli@users.noreply.github.com>
2020-04-20 20:30:25 -0300

    Update Main.java

--- Merge/src/main/java/com/facundo/merge/Main.java
+++ Merge/src/main/java/com/facundo/merge/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         Sum sum = new Sum();
-        System.out.println("This is a message");
         System.out.println(sum.calculate(15, 5));
+        System.out.println("This is a conflict");
+
     }
 }
