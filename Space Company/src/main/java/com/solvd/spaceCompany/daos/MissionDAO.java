bcc481b6ee5fd86424003406dc537d64d2604025
Facundo <c.facu98@gmail.com>
2020-05-22 09:06:40 -0300

    NOT finished DAOs

--- Space Company/src/main/java/com/solvd/spaceCompany/daos/MissionDAO.java
+++ Space Company/src/main/java/com/solvd/spaceCompany/daos/MissionDAO.java
@@ -6,10 +6,7 @@ import com.solvd.spaceCompany.models.Mission;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -97,7 +94,7 @@ public class MissionDAO implements IDAO<Mission> {
         Connection connection = ConnectionPool.getInstance().getConnection();
         try {
             connection.setAutoCommit(false);
-            PreparedStatement ps = connection.prepareStatement(INSERT_MISSION);
+            PreparedStatement ps = connection.prepareStatement(INSERT_MISSION, Statement.RETURN_GENERATED_KEYS);
             ps.setString(1, mission.getName());
             ps.setString(2, mission.getObjective());
             ps.setInt(3, mission.getSpan());
